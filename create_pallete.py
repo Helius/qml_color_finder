e7b9c24ff204ea04042e39be5eb6acb87ff5bfd3
Eugene Samoylov <e.samoylov@2gis.ru>
2017-05-19 11:32:14 +0700

    now we support ARGB color

--- create_pallete.py
+++ create_pallete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import random
 import math
 import colorsys
 import struct
@@ -8,7 +7,7 @@ import binascii
 
 html_table='<div style="width:100%; display:table; table-layout: fixed;">'
 html_row='<div style="display: table-row; height:20px">'
-html_color='<div style="width:20%; background-color:{}; display: table-cell"></div>'
+html_color='<div style="width:20%; background-color:{}; opacity:{}; display: table-cell"></div>'
 html_color_code='<div style="width:8%; padding-left:1%; display: table-cell">{}</div>'
 html_color_name='<div style="width:12%; display: table-cell">{}</div>'
 html_use_url='<div style="display: table-cell"><a href={}>{}</a></div>'
@@ -17,14 +16,23 @@ html_close_div='</div>'
 colors = []
 
 class ColorEntry:
-    
+
     def __init__(self, line):
-        self.filename, self.color, self.colorName = line.split(";")
-    
+        self.filename, self.colorCode, self.colorName = line.split(";")
+        if len(self.colorCode) > 7: # #33FFCCEE - we have opacity here
+            #print ('found opacity', hex_to_rgb(self.color))
+            #print('found color with opacity', self.color[1:-4], int(self.color[1:-4])/256.0)
+            self.opacity = (hex_to_rgb(self.colorCode))[0]/256.0
+            self.color = '#'+self.colorCode[3:]
+        else:
+            self.color = self.colorCode
+
     color = "#000000"
+    colorCode = "#00000000"
     filename = "filename"
     string = 0
-    colorName = "warmGray"
+    colorName = "undefined"
+    opacity = 1
 
 
 def read_color_to_list():
@@ -38,8 +46,8 @@ def print_colors(colors):
 	print (html_table)
 	for i in colors:
 		print (html_row)
-		print (html_color.format(i.color))
-		print (html_color_code.format(i.color))
+		print (html_color.format(i.color, i.opacity))
+		print (html_color_code.format(i.colorCode))
 		print (html_color_name.format(i.colorName))
 		#TODO: use urls
 		print (html_use_url.format(i.filename, i.filename))
